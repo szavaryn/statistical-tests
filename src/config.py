b4c3df365a56d8d21058ee3df5aade06de98c7b6
szavaryn <szavaryn@gmail.com>
2025-12-31 07:22:41 +0100

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,10 @@
+DEFAULT_COLORS = [
+    "blue", "yellow", "purple", "black", "green"
+    , "red", "orange", "pink", "gray", "cyan"
+]
+FIGSIZE = (15, 7)
+CI_RESAMPLES = 1000
+CI_LEVEL = 0.95
+ALPHA = 0.05
+BETA = 0.2
+DATA_PATH = ""
